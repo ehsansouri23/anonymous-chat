5c18fc33d7df6b05b20ad50fb81b9774bc27d6ed
Ehsan <ehsansouri23@gmail.com>
2019-06-25 00:42:23 +0430

    rename class. add todo

--- src/UdpAcceptListener.kt
+++ src/UdpAcceptListener.kt
@@ -1,17 +1,26 @@
 import java.net.DatagramPacket
 import java.net.DatagramSocket
+import java.net.InetAddress
 
 const val udpAcceptPort = 5000
+lateinit var address: InetAddress
+var port = 0
 
-class UdpListener : Thread() {
+/**
+ * listening for accept of broadcasted udp message
+ */
+class UdpAcceptListener : Thread() {
     private val socket = DatagramSocket(udpAcceptPort)
     override fun run() {
         println("listening")
         val bytes = ByteArray(512)
         val packet = DatagramPacket(bytes, bytes.size)
         socket.receive(packet)
+        address = packet.address
+        port = "123".toInt()//todo handle it
         println("Received ${String(bytes)}")
         println("---------------------")
         stopBroadcasting()
+        TcpClient().start()
     }
 }
