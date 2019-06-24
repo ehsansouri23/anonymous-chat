86a3937ad8272f342cf8ad2c24d4c4f2e787abde
Ehsan <ehsansouri23@gmail.com>
2019-06-24 21:47:14 +0430

    make udp broadcaster socket global

--- src/broadcast.kt
+++ src/broadcast.kt
@@ -2,9 +2,10 @@ import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 
+val socket = DatagramSocket()
+
 fun broadcastUdp(message: String) {
     println("Broadcasting ($message) udp message...")
-    val socket = DatagramSocket()
     socket.broadcast = true
     val bytes = message.toByteArray()
     val packet = DatagramPacket(bytes, bytes.size, InetAddress.getByName("255.255.255.255"), 4400)
