2e5d610195b1b86372c1da4baef1a2c1194d8d94
Ehsan <ehsansouri23@gmail.com>
2019-06-27 14:11:10 +0430

    start read and write thread after a new connection

--- src/tcp/TcpClient.kt
+++ src/tcp/TcpClient.kt
@@ -1,21 +1,14 @@
 package tcp
 
 import address
-import java.io.DataOutputStream
 import java.net.Socket
-import java.util.*
 
 class TcpClient(private val port: Int) : Thread("tcp.TcpClient") {
 
     override fun run() {
         println("Connecting to $address and port $port")
         val socket = Socket(address, port)
-        while (true) {
-            val out = DataOutputStream(socket.getOutputStream())
-            val scanner = Scanner(System.`in`)
-            val s = scanner.next()
-            println("send $s")
-            out.writeUTF(s)
-        }
+        WriteThread(socket).start()
+        ReadThread(socket).start()
     }
 }
--- src/tcp/TcpServer.kt
+++ src/tcp/TcpServer.kt
@@ -1,18 +1,15 @@
 package tcp
 
-import java.io.BufferedInputStream
-import java.io.DataInputStream
 import java.net.ServerSocket
 
 class TcpServer(private val port: Int) : Thread("tcp.TcpServer") {
 
     override fun run() {
         println("Establishing server on port $port")
-        val socket = ServerSocket(port)
-        while (true) {
-            val recievedSocket = socket.accept()
-            val inputStream = DataInputStream(BufferedInputStream(recievedSocket.getInputStream()))
-            println(inputStream.readLine())
-        }
+        val serverSocket = ServerSocket(port)
+        val socket = serverSocket.accept();
+        println("Socket accepted")
+        WriteThread(socket).start()
+        ReadThread(socket).start()
     }
 }
